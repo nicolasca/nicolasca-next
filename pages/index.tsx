976752b1111c738463a9618c0894616a3f1eaed8
Nicolas Castejon <ncastejon.externe@bedrockstreaming.com>
2022-09-30 15:26:45 +0200

    Create scroll button

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,6 @@ import FacePicture from "../components/svg/FacePicture";
 import FacePhoto from "../public/face-profile-photo.png";
 
 const HomeBlock = styled.div`
-  display: flex;
   width: 100%;
   height: 100%;
   background-color: #fff;
@@ -51,26 +50,65 @@ const Title = styled.div`
   justify-content: center;
   gap: 3rem;
   width: 100%;
+  height: 100%;
+
+  text-transform: uppercase;
+  position: relative;
 
   & > ${FaceWrapper}, & > ${Content} {
-    filter: blur(3px);
+    filter: blur(10px);
     transition: filter 0.5s ease;
 
     &:hover {
-      filter: blur(0px);
+      filter: blur(1px);
     }
   }
 `;
 
 const JobTitle = styled.h2`
   color: var(--matrix-green);
+  text-transform: uppercase;
 `;
 
 const CanvasWrapper = styled.div`
   position: absolute;
 `;
 
+const FollowBottom = styled.div`
+  position: absolute;
+  bottom: 0;
+  background: white;
+  padding: 1rem 2rem;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+
+  &:hover {
+    cursor: pointer;
+  }
+
+  .down-arrow {
+    border: solid black;
+    border-width: 0 3px 3px 0;
+    display: inline-block;
+    padding: 3px;
+    transform: rotate(45deg);
+  }
+`;
+
+const ProjectionSection = styled.div`
+  height: 100vh;
+`;
+
 const Home: NextPage = () => {
+  const scrollToBottom = () => {
+    const screenHeight = window.innerHeight;
+    const element = document.getElementById("section-2");
+    console.log("scroll", screenHeight);
+    element?.scrollIntoView({ behavior: "smooth" });
+    // window.scrollTo({top:-screenHeight, behavior: 'smooth'});
+  };
+
   return (
     <HomeBlock>
       <CanvasWrapper>
@@ -83,9 +121,22 @@ const Home: NextPage = () => {
 
         <Content>
           <h1>Nicolas Castejon</h1>
-          <JobTitle>Développeur Web</JobTitle>
+          <JobTitle>Developpeur Web</JobTitle>
         </Content>
+        <FollowBottom onClick={scrollToBottom}>
+          <span>Follow the white rabbit</span>
+          <span className="down-arrow"></span>
+        </FollowBottom>
       </Title>
+      <ProjectionSection id="section-2">
+        <div>more content to follow</div>
+        <div>more content to follow</div>
+        <div>more content to follow</div>
+      </ProjectionSection>
+      <div>
+        <h2>CREDITS</h2>
+        <span>Matrix effect: Ganesh Prasad - https://codepen.io/gnsp/pen/vYBQZJm</span>
+      </div>
     </HomeBlock>
   );
 };
